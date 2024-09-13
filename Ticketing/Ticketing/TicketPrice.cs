cb6628cbe5e14ed7be19bd6dc9d29058a6341b25
Ian Tiggeman <ian.tiggeman@student.tafesa.edu.au>
2024-09-13 22:54:35 +0930

    10% discount added to ticket discount

--- Ticketing/Ticketing/TicketPrice.cs
+++ Ticketing/Ticketing/TicketPrice.cs
@@ -17,7 +17,7 @@ namespace Ticketing
         const decimal mdecBalcony = 37.5m;
         const decimal mdecGeneral = 30.75m;
         const decimal mdecBox = 65.0m;
-        const decimal mdecDiscount = 5.0m; 
+        const decimal mdecDiscount = 15.0m; 
 
         private int Section
         {
